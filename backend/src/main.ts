a25ea944834f5c200930a274022f2068cb25267f
Navneetk4269 <navneetkumar57810@gmail.com>
2026-09-24 13:46:56 +0530

    Resolved github repo verification error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import dns from "node:dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
